ea950d933d0186a94c4f9ac0735eacdd885421c9
Artur Markov <artur.markov1860@gmail.com>
2019-09-07 13:18:19 -0700

    code updated

---
+++ hw.js
@@ -0,0 +1,24 @@
+function Shape(n, s) {
+    this.name = n;
+    this.sides = s;
+};
+
+function Rectangle(h,w, n, s) {
+    this.heigth = h;
+    this.width = w;
+    Shape.call(this, n, s);
+};
+
+Rectangle.prototype.area = function(){
+    console.log(this.heigth * this.width);
+};
+
+Rectangle.prototype.perimeter = function () {
+    console.log(this.heigth * 2 + this.width * 2);
+};
+
+function Square(h,n,s){
+    Rectangle.call(this, h, h,n,s)
+}
+
+Square.prototype = Object.create(Rectangle.prototype);
